7de09dbd257825f74ba52d53b0c81fd68862f381
James Villemarette <jvill@udel.edu>
2020-02-18 12:33:06 -0500

    initial commit

---
+++ tastylist/tastylist.h
@@ -0,0 +1,9 @@
+struct node;
+void append(int num);
+void add(int num);
+void addafter(int num, int loc);
+void insert(int num);
+int delete(int num);
+void display(struct node *r);
+int count();
+int main();
